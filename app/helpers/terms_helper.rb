bed619c0304a013256433347245f5586e3a85510
Bartosz Zurkowski <b.zurkowski93@gmail.com>
2015-04-24 11:51:52 +0200

    Distribute terms over schedule

--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -2,4 +2,15 @@ module TermsHelper
   def day_name(day_num)
     t(:"date.day_names")[day_num].to_s
   end
+
+  def term_position(term)
+    start_time = term.starts_at
+    minutes = (start_time.hour - 6) * 60 + start_time.min
+
+    (minutes / 5) * 5
+  end
+
+  def term_time_range(term)
+    "#{term.starts_at.strftime("%H:%M")} - #{term.ends_at.strftime("%H:%M")}"
+  end
 end
