4529ddb4c910c5e998de82aeb09f6a75738bbe3f
Bartosz Zurkowski <b.zurkowski93@gmail.com>
2015-05-15 05:23:31 +0200

    Add missing offer controller actions

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,11 +1,19 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: [:show, :edit, :update, :destroy]
+
   def index
     @filter = ::Filters::Offer.new user: current_user, subject_id: params[:subject_id]
     @offers = @filter.scope.decorate
   end
 
+  def show
+  end
+
   def new
     @offer = Offer.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,9 +26,27 @@ class OffersController < ApplicationController
     end
   end
 
+  def update
+    if @offer.update(offer_params)
+      redirect_to @offer
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @offer.destroy
+
+    redirect_to offers_path
+  end
+
   private
 
   def offer_params
     params.require(:offer).permit(:id, :assignation_id, requested_term_ids: [])
+  end
+
+  def set_offer
+    @offer = Offer.find(params[:id])
   end
 end
